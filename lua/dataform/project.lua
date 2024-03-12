2de0ab890cb3358e622a3a6f0275c151bbe45b97
Lucas Magalhaes <magal1337@Lucass-Air.intelbras.local>
2024-03-12 20:57:22 -0300

    adjust method to open new buffer and bq validation v3

--- lua/dataform/project.lua
+++ lua/dataform/project.lua
@@ -53,7 +53,7 @@ function M.get_compiled_sql_job()
 
   for _, table in pairs(tables) do
     if table.fileName == M.get_dataform_definitions_file_path() then
-      local bq_command = "bq query --dry_run " .. table.query
+      local bq_command = "bq query --dry_run '''" .. table.query .. "'''"
 
       local handle = io.popen(bq_command)
       local result = handle:read("*a")
