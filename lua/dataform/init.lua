45636e6a09b93de4741b4c2e5cfd8316a6d38adf
Lucas Magalhaes <magal1337@Lucass-Air.intelbras.local>
2024-03-16 14:49:27 -0300

    adding function to run specifc job file v13

--- lua/dataform/init.lua
+++ lua/dataform/init.lua
@@ -14,5 +14,6 @@ M.get_compiled_sql_job = project.get_compiled_sql_job
 M.get_compiled_sql_incremental_job = project.get_compiled_sql_incremental_job
 M.go_to_ref = project.go_to_ref
 M.dataform_run_action_job = project.dataform_run_action_job
+M.dataform_run_action_assertions = project.dataform_run_action_assertions
 
 return M
