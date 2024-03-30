032b211a168a86686b446f93b41510f79948724b
Lucas Magalhaes <magal1337@Lucass-Air.intelbras.local>
2024-03-30 12:03:10 -0300

    adding feature to adjust workdir automatically v7

--- lua/dataform/project.lua
+++ lua/dataform/project.lua
@@ -6,10 +6,10 @@ dataform.compiled_project_table = {}
 
 function dataform.set_dataform_workdir_project_path()
   local current_path = utils.get_current_file_path()
-  local parent_path = current_path:match("(.*/).*/definitions/")
-  parent_path = parent_path and parent_path or nil
+  local is_match = string.match(current_path, "/definitions/.*")
 
-  if parent_path then
+  if is_match then
+    local parent_path = current_path:gsub("/definitions/.*", "/")
     vim.api.nvim_set_current_dir(parent_path)
   else
     return vim.notify("Error: File does not exist inside dataform definitions folder.", 4)
