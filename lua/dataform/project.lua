aa12e63972d1032b5e5cdd2e6bf7d9fede85cd7a
Lucas Magalhaes <magal1337@Lucass-Air.intelbras.local>
2024-06-18 17:02:11 -0300

    testing telescope v17

--- lua/dataform/project.lua
+++ lua/dataform/project.lua
@@ -166,7 +166,8 @@ function dataform.find_model_dependencies()
   local tables = dataform.compiled_project_table.tables
   local operations = dataform.compiled_project_table.operations
   local declarations = dataform.compiled_project_table.declarations
-  local all_models = vim.fn.extend(tables, operations, declarations)
+  local bind_models = vim.fn.extend(tables, operations)
+  local all_models = vim.fn.extend(bind_models, declarations)
   local target_paths = {}
 
   for _, model in pairs(all_models) do
